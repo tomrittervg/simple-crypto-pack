20610a1affa96654f75c2f9997b26cb62892d0a5
Tom Ritter <tom@ritter.vg>
2010-01-16 00:31:28 -0500

    hangman improvements

--- hangman/hangman.php
+++ hangman/hangman.php
@@ -9,6 +9,19 @@ $dict = new dictionary();
 
 $offset = ord($ciphertext->getReferenceLetterA());
 
+function printreplacements($a)
+{
+  global $offset;
+  for($i=0;$i<26;$i++)
+    echo chr($i + $offset);
+  echo "\n";
+  for($i=0;$i<26;$i++)
+    if(in_array(chr($i + $offset), $a))
+      echo array_search(chr($i + $offset), $a);
+    else
+      echo ".";
+  echo "\n";
+}
 
 //time to make a shell
 $continueshell = true;
@@ -24,61 +37,63 @@ do
     switch($cmd)
       {
       case 'exit':
-	echo "Goodbye.\n";
-	$continueshell=false;
-	break;
+        echo "Goodbye.\n";
+        $continueshell=false;
+        break;
       case 'restart':
-	$tempguess = strtoupper($ciphertext);
-	$replacements = array();
-	echo $tempguess;
-	break;
+        $tempguess = strtoupper($ciphertext);
+        $replacements = array();
+        echo $tempguess;
+        break;
       case 'showlog':
-	print_r($replacements);
-	break;
+        print_r($replacements);
+        break;
       case 'help':
-	echo "Valid Commands:\n";
-	echo "\texit\n";
-	echo "\trestart\n";
-	echo "\tshowlog\n";
-	echo "\thelp\n";
-	echo "\tc=p\n";
-	break;
+        echo "Valid Commands:\n";
+        echo "\texit\n";
+        echo "\trestart\n";
+        echo "\tshowlog\n";
+        echo "\thelp\n";
+        echo "\tc=p\n";
+        break;
       case '':
       default:
-	{
-	  $sides = explode('=', $cmd);
-	  if(count($sides) != 2) 
-	    {
-	      echo "Invalid syntax\n";
-	      break;
-	    }
-	  $sides[0] = strtolower(trim($sides[0]));
-	  $sides[1] = strtolower(trim($sides[1]));
-	  if(strlen($sides[0]) != 1 || strlen($sides[1]) != 1)
-	    {
-	      echo "Invalid syntax\n";
-	      break;
-	    }
-	  if(isset($replacements[$sides[0]]))
-	    {
-	      echo "You already replaced the letter " . $sides[0] . "\n";
-	      break;
-	    }
-	  if(in_array($sides[1], $replacements))
-	    {
-	      echo "You already replaced " . array_search($sides[1], $replacements) . " with " . $sides[1] . "\n";
-	      $confirm = readline("Are you sure you want to replace again? [y/n]");
-	      if(trim($confirm) == "" || trim(strtolower($confirm)) == "y")
-		;
-	      else
-		break;
-	    }
+        {
+          $sides = explode('=', $cmd);
+          if(count($sides) != 2)
+            {
+              echo "Invalid syntax\n";
+              break;
+            }
+          $sides[0] = strtolower(trim($sides[0]));
+          $sides[1] = strtolower(trim($sides[1]));
+          if(strlen($sides[0]) != 1 || strlen($sides[1]) != 1)
+            {
+              echo "Invalid syntax\n";
+              break;
+            }
+          if(isset($replacements[$sides[0]]))
+            {
+              echo "You already replaced the letter " . $sides[0] . "\n";
+              break;
+            }
+          if(in_array($sides[1], $replacements))
+            {
+              echo "You already replaced " . array_search($sides[1], $replacements) . " with " . $sides[1] . "\n";
+              $confirm = readline("Are you sure you want to replace again? [y/n]");
+              if(trim($confirm) == "" || trim(strtolower($confirm)) == "y")
+                ;
+              else
+                break;
+            }
 
-	  $replacements[$sides[0]] = $sides[1];
-	  $tempguess = str_replace(strtoupper($sides[0]), $sides[1], $tempguess);
-	  echo $tempguess;
-
-	  break;
-	}
+          $replacements[$sides[0]] = $sides[1];
+          $tempguess = str_replace(strtoupper($sides[0]), $sides[1], $tempguess);
+          echo $tempguess;
+	  echo "\n";
+	  printreplacements($replacements);
+	  echo"\n";
+          break;
+        }
       }
   }    while($continueshell);
