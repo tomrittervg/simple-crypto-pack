c73dcc4764f25e1bd19dfb2edb33ce679b41b3eb
Tom Ritter <tom@ritter.vg>
2010-01-17 13:43:50 -0500

    usage help

--- common/common.php
+++ common/common.php
@@ -9,7 +9,7 @@ require("str-functions.php");
 
 $options = array();
 
-$tmp = getopt("vd", array("match-length:", "key:", "index-of-coincidence:", "index-only"));
+$tmp = getopt("vd", array("match-length:", "key:", "index-of-coincidence:", "index-only", "help"));
 
 $options['debug'] = isset($tmp['d']);
 $options['verbose'] = isset($tmp['v']);
@@ -17,5 +17,20 @@ $options['match-length'] = isset($tmp['match-length']) ? $tmp['match-length'] : 
 $options['index-of-coincidence'] = isset($tmp['index-of-coincidence']) ? $tmp['index-of-coincidence'] : 10;
 if(isset($tmp['key'])) $options['key'] = $tmp['key'];
 $options['index-only'] = isset($tmp['index-only']);
+
+if(isset($tmp['help']))
+  {
+    $padlen = 30;
+    echo "Usage:\n";
+    echo "\t" . str_pad("-v", $padlen) . "Verbose Mode\n";
+    echo "\t" . str_pad("-d", $padlen) . "Debug Mode\n";
+    echo "\n";
+    echo "\t" . str_pad("--match-length=n", $padlen) . "Minimum word length for a dictionary match\n";
+    echo "\t" . str_pad("--index-of-coincidence=n", $padlen) . "Go up to keylength of n when testing\n";
+    echo "\t" . str_pad("--key=value", $padlen) . "Test this key only\n";
+    echo "\t" . str_pad("--index-only", $padlen) . "only show the index of coincidence compustats\n";
+    
+    exit;
+  }
 
 unset($tmp);
--- common/str-functions.php
+++ common/str-functions.php
@@ -4,3 +4,11 @@ function strip_whitespace($s)
 {
   return str_replace(array(" ", "\r", "\t", "\n"), "", $s);
 }
+function is_whitespace($c)
+{
+  return
+    " " == $c ||
+    "\r" == $c ||
+    "\n" == $c ||
+    "\t" == $c;
+}
