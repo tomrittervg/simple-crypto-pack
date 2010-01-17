6d5332b1552b8fce87453e936ca88a7b4620efaf
Tom Ritter <tom@ritter.vg>
2010-01-17 14:41:56 -0500

    adding doubles

--- frequency/frequency.php
+++ frequency/frequency.php
@@ -9,12 +9,11 @@ $offset = ord($ciphertext->getReferenceLetterA());
 
 $table = get_frequency_array($ciphertext);
 
-if($options['verbose'])
-  print_r($table);
-
-// Yea, the graph grows down.  Cause there's no reason for it *not* to, and it's easier
-
-$max = max($table);
+if($options['debug'])
+  {
+    echo "\nFrequency Array:\n";  
+    print_r($table);
+  }
 
 echo "The cipher is " . strlen($ciphertext) . " characters long including whitespace.\n";
 echo "The cipher is " . strlen(strip_whitespace($ciphertext)) . " characters long without whitespace.\n";
@@ -25,17 +24,48 @@ print_alpha_frequency_graph($table, $offset);
 echo "\nNormal English Frequency:\n";
 print_alpha_frequency_graph($frequencies['normal-english'], ord('a'));
 
-$table = get_digraph_frequency_array($ciphertext);
-$max = max($table);
-$min = min($table);
-if($min + 2 > $max)
+//Digraphs
+$digraph_table = get_digraph_frequency_array($ciphertext);
+if($options['debug'])
+  {
+    echo "\nDigraph Array:\n";
+    print_r($digraph_table);
+  }
+$max = max($digraph_table);
+$min = min($digraph_table);
+if($min + 2 > $max && !$options['verbose'] && !$options['debug'])
   echo "\nDigraph Frequency is flat - not bothering...\n";
 else
   {
     echo "\nDigraphs:\n";
-    print_literal_frequency_graph($table, 2);
+    print_literal_frequency_graph($digraph_table, 2);
     
     echo "\nEnglish Digraphs:\n";
     print_literal_frequency_graph($frequencies['english-digraph'], 2);
   }
+
+//Doubles
+function isdouble($k) { return $k[0] == $k[1]; }
+$good_keys = array_filter(array_keys($digraph_table), "isdouble");
+$doubles_table = array();
+
+foreach($good_keys as $v)
+  $doubles_table[$v] = $digraph_table[$v];
+
+if($options['debug'])
+  {
+    echo "\nDoubles Array:\n";
+    print_r($doubles_table);
+  }
+if(count($doubles_table) >= 1)
+  {
+    if(count($doubles_table) > 1)
+      print_literal_frequency_graph($doubles_table, 2);
+    else
+      echo "Only one double found: " . key($doubles_table) . ", found " . current($doubles_table) . " time(s).\n";
+    echo "English doubles are most commonly: LL, EE, SS, OO, FF, TT.\n";
+  }
+else
+  echo "No doubled letters found...\n";
+
 echo "\n";
